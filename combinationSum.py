e6a641c2ea094819d012e5247e914ac9c5453382
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-08-29 08:35:25 -0500

    Update combinationSum.py

--- combinationSum.py
+++ combinationSum.py
@@ -3,3 +3,11 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i, cur, total):
+            if target == total:
+                res.append(cur.copy())
+                return
+            if i >= len(candidates) or target < total:
+                return
